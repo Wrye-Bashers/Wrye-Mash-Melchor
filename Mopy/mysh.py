32b5e7f5fa8210cf12876ea693185548f206f5eb
Sharlikran <danielhmpdx16@gmail.com>
2017-09-23 07:55:40 -0700

    Style and indent changes to mysh.py

--- Mopy/mysh.py
+++ Mopy/mysh.py
@@ -25,150 +25,173 @@
 import os
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # HTMLHelpPage
 #
 # file like object handling the conten of a help page
 class HTMLHelpPage(file):
-	"""Help page file-like class."""
-	def __init__(self, data):
-		"""data : str : html text"""
-		self.data = data
-		self._pos = 0
-		self._lineNum = 0
+    """Help page file-like class."""
 
-	def read(self, *args):
-		"""..."""
-		if len(args) != 0: 
-			sz = args[0]
-			pos = int(self._pos)
-			if self._pos == len(self.data) -1:
-				return ""
-			if sz <= len(self.data[pos:]):
-				self._pos += sz
-				return self.data[pos:self._pos]
-			else:
-				self._pos = len(self.data) -1
-				return self.data[pos:]
-		else:
-			return self.data[self._pos:]
-		return self.data
+    def __init__(self, data):
+        """data : str : html text"""
+        self.data = data
+        self._pos = 0
+        self._lineNum = 0
 
-	def readline(self, *args):
-		lines = self.data.splitlines()
-		if self._lineNum < len(lines):
-			line = lines[self._lineNum]
-			self._lineNum += 1
-			if len(args) != 0:
-				sz = args[0]
-				return line[:sz]
-			else:
-				return line
-		else:
-			return ""
+    def read(self, *args):
+        """..."""
+        if len(args) != 0:
+            sz = args[0]
+            pos = int(self._pos)
+            if self._pos == len(self.data) - 1:
+                return ""
+            if sz <= len(self.data[pos:]):
+                self._pos += sz
+                return self.data[pos:self._pos]
+            else:
+                self._pos = len(self.data) - 1
+                return self.data[pos:]
+        else:
+            return self.data[self._pos:]
+        return self.data
 
-	def write(self, *args, **kwargs):
-		"""..."""
-		pass
+    def readline(self, *args):
+        lines = self.data.splitlines()
+        if self._lineNum < len(lines):
+            line = lines[self._lineNum]
+            self._lineNum += 1
+            if len(args) != 0:
+                sz = args[0]
+                return line[:sz]
+            else:
+                return line
+        else:
+            return ""
+
+    def write(self, *args, **kwargs):
+        """..."""
+        pass
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # HTMLHelpParser
 #
 # This class is intended to parse an HTML file and retrieve some tags.
 class HTMLHelpParser(file):
-	"""Handling, parsing and hashing HTML files."""
-	def __init__(self):
-		"""..."""
-		self.src = None # str : html file path + name
-		self.raw_data = None # str : html file content
-		self.content = None # str : html table of content
-		self.pages = None # dict : keys :  str) : page name ; values : str : page html text
+    """Handling, parsing and hashing HTML files."""
 
-	def SetSrc(self, src):
-		"""Set the html source path.
+    def __init__(self):
+        """..."""
+        self.src = None  # str : html file path + name
+        self.raw_data = None  # str : html file content
+        self.content = None  # str : html table of content
+        self.pages = None  # dict : keys :  str) : page name ; values : str : page html text
 
-			src : str : html file path + name
-		"""
-		self.src = src
+    def SetSrc(self, src):
+        """Set the html source path.
 
-	def ReadData(self):
-		"""Reads the html source file."""
-		f = open(self.src, "r")
-		self.raw_data = f.read()
-		f.close()
+            src : str : html file path + name
+        """
+        self.src = src
 
-	def Parse(self):
-		"""Parses the raw data and fills up the content and the pages."""
-		# the first line is assumed to starts with <!DOCTYPE
-		preHead = self.raw_data.splitlines()[0]
-		# retrieve the sections
-		# head
-		head = self.raw_data[self.raw_data.lower().find("<head>"):self.raw_data.lower().find("</head>") + len("</head>")]
-		# body
-		body = self.raw_data[self.raw_data.lower().find("<body>"):self.raw_data.lower().find("</body>") + len("</body>")]
-		# table of content
-		start = body.lower().find("""<a name="tableofcontent"></a>""") and body.lower().find("""<a name='tableofcontent'></a>""")
-		end = min(len(body), (body.lower().find("""<a name="tableofcontent_end"></a>""") and body.lower().find("""<a name='tableofcontent_end'></a>""")) + (len("""<a name="tableofcontent_end"></a>""") and len("""<a name='tableofcontent_end'></a>""")))
-		self.content = preHead + "<html>" + head + "<body>" + body[start:end] + "</body></html>"
-		# pages
-		rawPages = body[body.lower().find("""<a name="pages"></a>""") + len("""<a name="pages"></a>"""):body.lower().find("""<a name="pages_end"></a>""")]
-		# parse the hrefs of the table of content and retrieve only the internal doc refs
-		hrefIndex = self.content.lower().find("""<p class=list-1>&bull;&nbsp; <a href="#""")
-		if hrefIndex == -1:
-			raise "Empty TOC"
-		endPrevPage = 0
-		# build up the pages
-		self.pages = {}
-		while hrefIndex != -1:
-			hrefEnd = self.content.find(">", hrefIndex + len("""<p class=list-1>&bull;&nbsp; <a href="#""") - 2)
-			pageRef = self.content[hrefIndex + len("""<p class=list-1>&bull;&nbsp; <a href="#""") - 2:hrefEnd]
-			pageName = pageRef.replace("#","")
-			_rawPages = rawPages[endPrevPage:]
-			pageIndex = _rawPages.find(pageName.strip(""" " ' """))
-			hrefIndex = self.content.lower().find("""<p class=list-1>&bull;&nbsp; <a href="#""", hrefEnd)
-			pageBegin = _rawPages.lower().find("<h2>")
-			pageEnd =  _rawPages[pageBegin+ 4 :].lower().find("<h2>")
-			if pageEnd != -1:
-				pageEnd = _rawPages[pageBegin+ 4 :].lower().find("<h2>") + pageBegin + 4
-			endPrevPage += pageEnd
-			page = _rawPages[pageBegin:pageEnd]
-			# correcting images addresses
-			if "<img " in page:
-				page = page.replace("""<img src="images/""", """<img src="%s\\"""%os.path.join(os.getcwd(), "images"))
-			# add the header and footer to the page data
-			self.pages[pageName] = HTMLHelpPage(preHead + "<html>" + head + "<body>" + page + "</body></html>")
+    def ReadData(self):
+        """Reads the html source file."""
+        f = open(self.src, "r")
+        self.raw_data = f.read()
+        f.close()
 
-	def read(self, *args, **kwargs):
-		return self.content
+    def Parse(self):
+        """Parses the raw data and fills up the content and the pages."""
+        # the first line is assumed to starts with <!DOCTYPE
+        preHead = self.raw_data.splitlines()[0]
+        # retrieve the sections
+        # head
+        head = self.raw_data[
+        self.raw_data.lower().find("<head>"):self.raw_data.lower().find(
+            "</head>") + len("</head>")]
+        # body
+        body = self.raw_data[
+        self.raw_data.lower().find("<body>"):self.raw_data.lower().find(
+            "</body>") + len("</body>")]
+        # table of content
+        start = body.lower().find(
+            """<a name="tableofcontent"></a>""") and body.lower().find(
+            """<a name='tableofcontent'></a>""")
+        end = min(len(body), (body.lower().find(
+            """<a name="tableofcontent_end"></a>""") and body.lower().find(
+            """<a name='tableofcontent_end'></a>""")) + (len(
+            """<a name="tableofcontent_end"></a>""") and len(
+            """<a name='tableofcontent_end'></a>""")))
+        self.content = preHead + "<html>" + head + "<body>" + body[
+        start:end] + "</body></html>"
+        # pages
+        rawPages = body[body.lower().find("""<a name="pages"></a>""") + len(
+            """<a name="pages"></a>"""):body.lower().find(
+            """<a name="pages_end"></a>""")]
+        # parse the hrefs of the table of content and retrieve only the internal doc refs
+        hrefIndex = self.content.lower().find(
+            """<p class=list-1>&bull;&nbsp; <a href="#""")
+        if hrefIndex == -1:
+            raise "Empty TOC"
+        endPrevPage = 0
+        # build up the pages
+        self.pages = {}
+        while hrefIndex != -1:
+            hrefEnd = self.content.find(">", hrefIndex + len(
+                """<p class=list-1>&bull;&nbsp; <a href="#""") - 2)
+            pageRef = self.content[hrefIndex + len(
+                """<p class=list-1>&bull;&nbsp; <a href="#""") - 2:hrefEnd]
+            pageName = pageRef.replace("#", "")
+            _rawPages = rawPages[endPrevPage:]
+            pageIndex = _rawPages.find(pageName.strip(""" " ' """))
+            hrefIndex = self.content.lower().find(
+                """<p class=list-1>&bull;&nbsp; <a href="#""", hrefEnd)
+            pageBegin = _rawPages.lower().find("<h2>")
+            pageEnd = _rawPages[pageBegin + 4:].lower().find("<h2>")
+            if pageEnd != -1:
+                pageEnd = _rawPages[pageBegin + 4:].lower().find(
+                    "<h2>") + pageBegin + 4
+            endPrevPage += pageEnd
+            page = _rawPages[pageBegin:pageEnd]
+            # correcting images addresses
+            if "<img " in page:
+                page = page.replace("""<img src="images/""",
+                    """<img src="%s\\""" % os.path.join(os.getcwd(), "images"))
+            # add the header and footer to the page data
+            self.pages[pageName] = HTMLHelpPage(
+                preHead + "<html>" + head + "<body>" + page + "</body></html>")
 
-	def write(self, *args, **kwargs):
-		pass
+    def read(self, *args, **kwargs):
+        return self.content
 
-	def GetPage(self, name):
-		"""Returs the page data for 'name', or the page containing 'name'.
-		If not found, returns the pair None, None."""
-		n = '"%s"'%name
-		N = "'%s'"%name
-		page = None
-		subpage = None
-		if n in self.pages.keys():
-			page = self.pages[n]
-		else:
-			for p, v in self.pages.iteritems():
-				if (v.read().find("name=%s"%n) != -1 or v.read().find("name=%s"%N) != -1) == True:
-					page =  self.GetPage(p[1:-1])
-					subpage = name
-		return page, subpage
+    def write(self, *args, **kwargs):
+        pass
+
+    def GetPage(self, name):
+        """Returs the page data for 'name', or the page containing 'name'.
+        If not found, returns the pair None, None."""
+        n = '"%s"' % name
+        N = "'%s'" % name
+        page = None
+        subpage = None
+        if n in self.pages.keys():
+            page = self.pages[n]
+        else:
+            for p, v in self.pages.iteritems():
+                if (v.read().find("name=%s" % n) != -1 or v.read().find(
+                        "name=%s" % N) != -1) == True:
+                    page = self.GetPage(p[1:-1])
+                    subpage = name
+        return page, subpage
+
 
 if __name__ == "__main__":
-	hhp = HTMLHelpParser()
-	hhp.SetSrc("content.html")
-	hhp.ReadData()
-	hhp.Parse()
-	a = ""
-	for b in hhp.pages.keys():
-		a += "\t%s\n"%b
-	print "Pages names:\n\n%s"%a
-	raw_input("Press ``Enter`` to quit.")
-
+    hhp = HTMLHelpParser()
+    hhp.SetSrc("content.html")
+    hhp.ReadData()
+    hhp.Parse()
+    a = ""
+    for b in hhp.pages.keys():
+        a += "\t%s\n" % b
+    print "Pages names:\n\n%s" % a
+    raw_input("Press ``Enter`` to quit.")
